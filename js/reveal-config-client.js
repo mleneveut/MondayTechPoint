d952b99753fc756a76c684acd350e80c226e9b08
Florian Lopes <flopes@ippon.fr>
2014-02-14 17:45:45 +0100

    Base project

---
+++ js/reveal-config-client.js
@@ -0,0 +1,33 @@
+// Full list of configuration options available here:
+// https://github.com/hakimel/reveal.js#configuration
+Reveal.initialize({
+    controls: true,
+    progress: true,
+    history: true,
+    center: true,
+
+    theme: Reveal.getQueryHash().theme, // available themes are in /css/theme
+    transition: Reveal.getQueryHash().transition || 'default', // default/cube/page/concave/zoom/linear/fade/none
+
+    // Parallax scrolling
+    // parallaxBackgroundImage: 'https://s3.amazonaws.com/hakim-static/reveal-js/reveal-parallax-1.jpg',
+    // parallaxBackgroundSize: '2100px 900px',
+
+    multiplex: {
+        // Example values. To generate your own, see the socket.io server instructions.
+        secret: null, // Obtained from the socket.io server. Gives this (the master) control of the presentation
+        id: '6c33a488a7abd3df', // Obtained from socket.io server
+        url: '127.0.0.1:1948' // Location of socket.io server
+    },
+
+    // Optional libraries used to extend on reveal.js
+    dependencies: [
+        { src: '//cdnjs.cloudflare.com/ajax/libs/socket.io/0.9.10/socket.io.min.js', async: true },
+        { src: 'plugin/multiplex/client.js', async: true },
+        { src: 'lib/js/classList.js', condition: function() { return !document.body.classList; } },
+        { src: 'plugin/markdown/marked.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: 'plugin/markdown/markdown.js', condition: function() { return !!document.querySelector( '[data-markdown]' ); } },
+        { src: 'plugin/highlight/highlight.js', async: true, callback: function() { hljs.initHighlightingOnLoad(); } },
+        { src: 'plugin/zoom-js/zoom.js', async: true, condition: function() { return !!document.body.classList; } }
+    ]
+});
