acbbd421e976230fbaa0e108fb48dd765cebafa6
Florian Lopes <flopes@ippon.fr>
2014-02-17 09:43:54 +0100

    Fixed : production url

--- js/reveal-config.js
+++ js/reveal-config.js
@@ -17,7 +17,7 @@ Reveal.initialize({
         // Example values. To generate your own, see the socket.io server instructions.
         secret: '13921084506082212166', // Obtained from the socket.io server. Gives this (the master) control of the presentation
         id: '6c33a488a7abd3df', // Obtained from socket.io server
-        url: '10.55.0.79:1948' // Location of socket.io server
+        url: 'http://coding-dojo.ippon-technologies.net:1948' // Location of socket.io server
     },
 
     // Optional libraries used to extend on reveal.js
